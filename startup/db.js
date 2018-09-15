aea0f38a0a8e505889a8bb4551b052c231ded5c9
Kamil Dąbrowski <Kamil@iMac-Kamil.local>
2018-09-15 18:29:18 +0200

    final commmit

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 
 module.exports = function() {
-  mongoose.connect('mongodb://localhost/kamil')
+  mongoose.connect(process.env.kamil_db)
     .then(() => {winston.info('Connected to MongoDB...')})
     .catch((err) => {
       console.log("Not Connected to Database ERROR!", err);
