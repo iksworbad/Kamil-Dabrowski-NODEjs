d5128cf8693c03db1e2dabbd7e706c7143d97a90
Kamil Dąbrowski <kamildabrowski@iMac-Kamil.local>
2018-10-19 00:05:59 +0200

    add .env and clean code

--- routes/movies.js
+++ routes/movies.js
@@ -1,41 +1,48 @@
-const {Movie, validateMovie} = require('../models/movie'); 
-const mongoose = require('mongoose');
-const express = require('express');
+const { Movie, validateMovie } = require("../models/movie");
+const mongoose = require("mongoose");
+const express = require("express");
 const router = express.Router();
-const request = require('request');
-const functions = require('../middleware/function');
+const request = require("request");
+const functions = require("../middleware/function");
 
-router.get('/', async (req, res) => {
-  const movies = await Movie.find().select({Title: 1, Year: 1, Runtime: 1, Genre: 1})
-                            .sort({Title: -1});
-                            
+router.get("/", async (req, res) => {
+  const movies = await Movie.find()
+    .select({ Title: 1, Year: 1, Runtime: 1, Genre: 1 })
+    .sort({ Title: -1 });
+
   res.send(movies);
 });
 
-
-
-router.post('/',  (req, res)=> {
-  const { error } = validateMovie(req.body); 
+router.post("/", (req, res) => {
+  const { error } = validateMovie(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const nameplus = functions.replaceSpace(req.body.title);
-  request.get('http://www.omdbapi.com/?apikey=c17d8385&plot=full&t='+nameplus, { json: true }, async (err, ress, body) => {
-  if (err) { return console.log(err); }
-     console.log(body);
-    const movie = new Movie(body);
-     await movie.save();
-     res.send(body);
-    });
-    
+  request.get(
+    "http://www.omdbapi.com/?apikey=" +
+      process.env.key +
+      "&plot=full&t=" +
+      nameplus,
+    { json: true },
+    async (err, ress, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(body);
+      const movie = new Movie(body);
+      await movie.save();
+      res.send(body);
+    }
+  );
 });
 
-
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const movie = await Movie.findById(req.params.id);
-    
-  if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+
+  if (!movie)
+    return res.status(404).send("The movie with the given ID was not found.");
 
   res.send(movie);
 });
 
-module.exports = router; 
+module.exports = router;
--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,13 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
+const winston = require("winston");
+const mongoose = require("mongoose");
 
 module.exports = function() {
-  mongoose.connect(process.env.kamil_db)
-    .then(() => {winston.info('Connected to MongoDB...')})
-    .catch((err) => {
+  mongoose
+    .connect(process.env.kamil_db)
+    .then(() => {
+      winston.info("Connected to MongoDB...");
+    })
+    .catch(err => {
       console.log("Not Connected to Database ERROR!", err);
     });
-}
+};
